9a704c1ce516974084d5c42ace254e6242960301
rizkaastengels <rizkyadityabgr@gmail.com>
2025-01-16 15:20:52 +0700

    Update dpph_calculator.py

--- DPPH faidh/dpph_calculator.py
+++ DPPH faidh/dpph_calculator.py
@@ -1,9 +1,3 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-
 # Halaman utama
 st.title("Kalkulator Antioksidan DPPH")
 st.sidebar.title("Navigasi")
